bbfea095fc0b216c3cdd688eb6ab6c8469193783
Guotong Liao <815190191@qq.com>
2022-01-03 12:55:55 +0800

    2022-01-03 Update

---
+++ 141. Linked List Cycle/Solution.java
@@ -0,0 +1,34 @@
+/**
+ * determine if the licked list has cycle
+ */
+
+ /**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+  public boolean hasCycle(ListNode head) {
+    // edge case
+    if (head == null && head.next == null) return false;
+
+    // 快慢指针
+    ListNode fast = head;
+    ListNode slow = head;
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+      if (fast == slow) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+}
