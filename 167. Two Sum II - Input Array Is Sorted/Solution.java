ba7baebd60b17c673e0d0d04199e5b6d102b1618
Guotong Liao <815190191@qq.com>
2022-01-05 21:32:34 +0800

    2022-01-05 Update

---
+++ 167. Two Sum II - Input Array Is Sorted/Solution.java
@@ -0,0 +1,13 @@
+import javax.print.attribute.standard.NumberUpSupported;
+
+public class Solution {
+  public int[] twoSum(int[] numbers, int target) {
+    int i = 0, j = numbers.length - 1;
+    while (i < j) {
+      if (numbers[i] + numbers[j] > target) j--;
+      if (numbers[i] + numbers[j] < target) i++;
+      if (numbers[i] + numbers[j] == target) break;
+    }
+    return new int[]{i+1, j+1};
+  }
+}
