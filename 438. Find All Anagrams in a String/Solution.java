94e6ac8c32385d6b52bb686410da57f3267aeb2a
Guotong Liao <815190191@qq.com>
2022-02-02 19:01:45 +0800

    2022-02-02 Update

--- 438. Find All Anagrams in a String/Solution.java
+++ 438. Find All Anagrams in a String/Solution.java
@@ -1,50 +1,51 @@
-public class Solution {
-  public List<Integer> findAnagrams(String s, String t) {
-      List<Integer> result = new LinkedList<>();
-      if(t.length()> s.length()) return result;
-      Map<Character, Integer> map = new HashMap<>();
+class Solution {
+  public List<Integer> findAnagrams(String s, String p) {
+      int sLen = s.length();
+      int pLen = p.length();
+      List<Integer> result = new ArrayList<>();
       
-      for(char c : t.toCharArray()){
+      if (sLen < pLen) return result;
+      
+      Map<Character, Integer> map = new HashMap<>();
+      for (char c : p.toCharArray()) {
           map.put(c, map.getOrDefault(c, 0) + 1);
       }
       
-      int counter = map.size();
+      // count 维护的是「需要匹配的不同字母的数量」
+      // count == 0 时，代表 map 里面所有字符对应数值都 <= 0 了
+      int count = map.size();
+      int l = 0, r = 0;
       
-      int begin = 0, end = 0;
-      int head = 0;
-      int len = Integer.MAX_VALUE;
-      
-      
-      while(end < s.length()){
-          char c = s.charAt(end);
+      while (r < sLen) {
           
-          if( map.containsKey(c) ){
-              map.put(c, map.get(c)-1);
-              if(map.get(c) == 0) counter--;
+          char c = s.charAt(r);
+          if (map.containsKey(c)) {
+              map.put(c, map.get(c) - 1);
+              if (map.get(c) == 0) count--;
           }
           
-          end++;
+          r++;
           
-          while(counter == 0){
-              char tempc = s.charAt(begin);
-              
-              if(map.containsKey(tempc)){
-                  map.put(tempc, map.get(tempc) + 1);
-                  if(map.get(tempc) > 0){
-                      counter++;
-                  }
+          // 只有「需要匹配的不同字母的数量」都为 0 时，才会进入这个循环
+          while (count == 0) {
+              char temp = s.charAt(l);
+              if (map.containsKey(temp)) {
+                  map.put(temp, map.get(temp) + 1);
+                  // 这个地方可能会出现某个字符对应值为负数或 0 的情况
+                  // 说明「窗口内的字符比需要匹配的字符多」，这种情况不加 count
+                  // 而只是将这个字符推出窗口
+                  if (map.get(temp) > 0) count++;
               }
               
-              if(end-begin == t.length()){
-                  result.add(begin);
+              if (r - l == pLen) {
+                  result.add(l);
               }
-              System.out.println("in while " + begin + " " + end + " " + counter);
-              begin++;
               
+              l++;
           }
-          System.out.println(begin + " " + end + " " + counter);
           
       }
+      
       return result;
   }
 }
