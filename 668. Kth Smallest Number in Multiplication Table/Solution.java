400e9fd2522fc50917976506075e3ad8af9ec057
Guotong Liao <815190191@qq.com>
2022-02-27 16:22:57 +0800

    2020-02-27 Update

--- 668. Kth Smallest Number in Multiplication Table/Solution.java
+++ 668. Kth Smallest Number in Multiplication Table/Solution.java
@@ -1,32 +1,27 @@
 class Solution {
-  public int findKthNumber(int m, int n, int k) {
-      int l = 1, r = m * n;
+    public int findKthNumber(int m, int n, int k) {
+        int l = 1, r = m * n;
       
-      while (l < r) {
-          int mid = l + r >> 1;
-          if (check(mid, m, n) >= k) {
-              r = mid;
-          } else {
-              l = mid + 1;
-          }
-      }
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (check(mid, m, n) >= k) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
       
-      return l;
-  }
+        return l;
+    }
   
-  private int check(int mid, int m, int n) {
-      int count = 0;
+    private int check(int mid, int m, int n) {
+        int count = 0;
       
-      int i = m, j = 1;
-      while (i >= 1 && j <= n) {
-          if (i * j > mid) {
-              i--;
-          } else {
-              count += i;
-              j++;
-          }
-      }
+        for (int i = 1, j = n; i <= m; i++) {
+            while (i * j > mid) j--;
+            count += j;
+        }
       
       return count;
-  }
+    }
 }
