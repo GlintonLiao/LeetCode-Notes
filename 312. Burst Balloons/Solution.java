1c047c581dbdf3a85e3ba247b3268c77773b8c7c
Guotong Liao <815190191@qq.com>
2022-04-01 22:15:31 +0800

    2022-04-01 Update

---
+++ 312. Burst Balloons/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        // 添加两侧的虚拟气球
+        int[] points = new int[n + 2];
+        points[0] = points[n + 1] = 1;
+        for (int i = 1; i <= n; i++) {
+            points[i] = nums[i - 1];
+        }
+        // base case 已经都被初始化为 0
+        int[][] f = new int[n + 2][n + 2];
+        // 开始状态转移
+        // i 应该从下往上
+        for (int i = n; i >= 0; i--) {
+            // j 应该从左往右
+            for (int j = i + 1; j < n + 2; j++) {
+                // 最后戳破的气球是哪个？
+                for (int k = i + 1; k < j; k++) {
+                    // 择优做选择
+                    f[i][j] = Math.max(
+                            f[i][j],
+                            f[i][k] + f[k][j] + points[i] * points[j] * points[k]
+                    );
+                }
+            }
+        }
+        return f[0][n + 1];
+    }
+}
