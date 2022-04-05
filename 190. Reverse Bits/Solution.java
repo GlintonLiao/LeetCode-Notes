1bf4de870ad395fa447858d9ccf81477f6e2a154
Guotong Liao <815190191@qq.com>
2022-04-05 10:41:14 +0800

    2022-04-05 Update

--- 190. Reverse Bits/Solution.java
+++ 190. Reverse Bits/Solution.java
@@ -2,9 +2,9 @@ public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
         int res = 0;
-        for (int i = 1; i < 32; i++) {
-            res += (n & 1);
+        for (int i = 0; i < 32; i++) {
             res <<= 1;
+            res += (n & 1);
             n >>= 1;
         }
         return res;
