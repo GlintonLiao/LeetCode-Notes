6e728972efdbd758d9977b6a4126e94cbc9e11cf
Guotong Liao <815190191@qq.com>
2022-04-06 20:49:21 +0800

    2022-04-06 Update

---
+++ 211. Design Add and Search Words Data Structure/Solution.java
@@ -0,0 +1,49 @@
+class WordDictionary {
+    
+    int[][] son = new int[(int)1e5 + 10][26];
+    int[] cnt = new int[(int)1e5 + 10];
+    int idx = 0;
+
+    public WordDictionary() {
+    }
+    
+    public void addWord(String word) {
+        int p = 0;
+        for (char c : word.toCharArray()) {
+            int u = c - 'a';
+            if (son[p][u] == 0) son[p][u] = ++idx;
+            p = son[p][u];
+        }
+        cnt[p]++;
+    }
+    
+    public boolean search(String word) {
+        char[] chs = word.toCharArray();
+        return isMatch(chs, 0, 0);
+    }
+    
+    private boolean isMatch(char[] chs, int i, int p) {
+        int n = chs.length;
+        if (i == n) return cnt[p] != 0;
+        if (chs[i] != '.') {
+            int u = chs[i] - 'a';
+            return son[p][u] != 0 && isMatch(chs, i + 1, son[p][u]);
+        } else {
+            for (int j = 0; j < 26; j++) {
+                if (son[p][j] != 0) {
+                    if (isMatch(chs, i + 1, son[p][j])) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
