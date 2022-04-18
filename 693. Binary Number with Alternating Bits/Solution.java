362dcf63e76d55f634bd93f550c75954ddee554a
Guotong Liao <815190191@qq.com>
2022-04-18 20:05:00 +0800

    2022-04-18 Update

---
+++ 693. Binary Number with Alternating Bits/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        int prev = n & 1;
+        n >>= 1;
+        while (n != 0) {
+            if ((n & 1) == prev) {
+                return false;
+            } else {
+                prev = n & 1;
+                n >>= 1;
+            }
+        }
+        return true;
+    }
+}
