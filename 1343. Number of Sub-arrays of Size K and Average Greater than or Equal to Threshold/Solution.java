956054fc9f0e7b813c2ff33c0ed55166675cb79b
Guotong Liao <815190191@qq.com>
2022-05-20 22:43:09 +0800

    2022-05-20 Update

--- 1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold/Solution.java
+++ 1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold/Solution.java
@@ -1,3 +1,19 @@
-public class Solution {
-  
+class Solution {
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+        int n = arr.length;
+        int[] sum = new int[n + 1];
+        
+        for (int i = 1; i <= n; i++) {
+            sum[i] = sum[i - 1] + arr[i - 1];
+        }
+        
+        int res = 0;
+        for (int i = k; i <= n; i++) {
+            if (sum[i] - sum[i - k] >= threshold * k) {
+                res++;
+            }
+        }
+        
+        return res;
+    }
 }
