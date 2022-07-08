40c05df0cdeccaeb625ef15e42827093a8462f61
Guotong Liao <815190191@qq.com>
2022-07-08 19:22:09 +0800

    2022-07-08 Update

---
+++ 503. Next Greater Element II/Solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        nums.insert(nums.end(), nums.begin(), nums.end());
+        stack<int> stk;
+        vector<int> res(n);
+        
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            int x = nums[i];
+            while (!stk.empty() && x >= stk.top()) stk.pop();
+            if (i < n) {
+                if (stk.empty()) res[i] = -1;
+                else res[i] = stk.top();
+            }
+            stk.push(x);
+        }
+        return res;
+    }
+};
