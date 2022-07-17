4d7a885717ea8808d6922ff757621e5989e46943
Guotong Liao <815190191@qq.com>
2022-07-17 08:50:17 +0800

    2022-07-17 Update

---
+++ 629. K Inverse Pairs Array/Solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int kInversePairs(int n, int k) {
+        const int MOD = 1e9 + 7;
+        // f[i, j] 表示有 i 个数，其中有 j 个逆序对的方案数量
+        vector<vector<int>> f(n + 1, vector<int>(k + 1));
+        f[1][0] = 1;
+        for (int i = 2; i <= n; i++) {
+            long long s = 0;
+            for (int j = 0; j <= k; j++) {
+                s += f[i - 1][j];
+                if (j - i >= 0) s -= f[i - 1][j - i];
+                f[i][j] = s % MOD;
+            }
+        }
+        return (f[n][k] + MOD) % MOD;
+    }
+};
